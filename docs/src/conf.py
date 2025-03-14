a39eb30de5ed674bba01cf52fbafee84f4040283
Adam Heins <mail@adamheins.com>
2025-03-14 11:23:55 -0400

    Bump to version 0.7.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -25,7 +25,7 @@ copyright = '2025, Adam Heins'
 author = 'Adam Heins'
 
 # The short X.Y version
-version = '0.6.0'
+version = '0.7.0'
 # The full version, including alpha/beta/rc tags
 release = version
 
